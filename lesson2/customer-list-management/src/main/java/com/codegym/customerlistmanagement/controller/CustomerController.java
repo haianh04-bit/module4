c9e6a34e0f50b210c85b12d03e14cf30c13c7e9e
phạm hải anh <phamhaianhpc10@gmail.com>
2025-07-16 22:46:48 +0700

    module4

--- lesson2/customer-list-management/src/main/java/com/codegym/customerlistmanagement/controller/CustomerController.java
+++ lesson2/customer-list-management/src/main/java/com/codegym/customerlistmanagement/controller/CustomerController.java
@@ -1,4 +1,39 @@
 package com.codegym.customerlistmanagement.controller;
 
+import com.codegym.customerlistmanagement.model.Customer;
+import com.codegym.customerlistmanagement.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customers")
 public class CustomerController {
+    @Autowired
+    private ICustomerService customerService;
+
+    @GetMapping()
+    public ModelAndView showList() {
+        ModelAndView modelAndView = new ModelAndView("list");
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView showInformation(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("info");
+        Customer customer = customerService.findById(id);
+        modelAndView.addObject("customer", customer);
+        return modelAndView;
+    }
+
+    @PostMapping
+    public String updateCustomer(Customer customer) {
+        customerService.save(customer);
+        return "redirect:/customers";
+    }
 }
